3835679d1b01919f1a14b7e071f0bb91e7987c84
Armen Avetisyan <aavetisyan@fb.com>
2021-03-23 09:19:06 +0000

    Densifying viewer layout

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,13 +1,9 @@
 <template>
-  <q-toolbar>
-    <q-btn icon="fas fa-brain" class="q-mr-sm" color="primary" to="/" flat dense> </q-btn>
-    <q-toolbar-title> <q-btn color="primary" label="Big Viz" size="lg" dense flat/>
+  <q-toolbar style='height:30px'>
+    <q-btn icon='fas fa-brain' class='q-mr-sm' color='primary' to='/' flat dense/>
+    <q-toolbar-title>
+      <q-btn color='primary' label='Big-Viz' size='lg' dense flat/>
     </q-toolbar-title>
-
-    <q-space> </q-space>
-    <div>
-      <q-btn to="/Reset" class="text-bold q-pa-sm" size="lg" flat dense no-caps> Reset </q-btn>
-    </div>
   </q-toolbar>
 </template>
 
@@ -24,5 +20,5 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang='scss'>
 </style>
--- client/src/pages/Viewer.vue
+++ client/src/pages/Viewer.vue
@@ -1,17 +1,20 @@
 <template>
   <q-page>
-
     <!-- search bar -->
     <div class='row q-pa-sm' id='div_menu_bar'>
       <div class='row items-center q-col-gutter-md' style='width:100%'>
         <div class='col-11'>
-          <q-input class='q-pa-none' v-model='search_text' label='File Path' input-class='text-bold' standout='bg-primary text-white' rounded bottom-slots>
-            <template v-slot:prepend> <q-icon name='fas fa-file' /> </template>
-            <template v-slot:append> <q-icon name='fas fa-times' @click='search_text = ""' class='cursor-pointer' /> </template>
+          <q-input class='q-pa-none' v-model='search_text' label='File Path' input-class='text-bold' standout='bg-primary text-white' rounded bottom-slots dense>
+            <template v-slot:prepend>
+              <q-icon name='fas fa-file' />
+            </template>
+            <template v-slot:append>
+              <q-icon name='fas fa-times' @click='search_text = ""' class='cursor-pointer' />
+            </template>
           </q-input>
         </div>
         <div class='col-1'>
-          <q-btn @click='onclick_grab' label='Grab' class='text-bold fit' color='primary' icon='fas fa-arrow-circle-right' :disable='search_text == ""'>  </q-btn>
+          <q-btn @click='onclick_grab' label='Grab' class='text-bold fit' color='primary' icon='fas fa-arrow-circle-right' :disable='search_text == ""' dense />
         </div>
       </div>
     </div>
@@ -21,13 +24,17 @@
     <div class='row q-pa-sm' id='div_status_bar'>
       <div class='flex items-center q-col-gutter-md' style='width:100%'>
         <div class='q-gutter-sm'>
-          <q-btn :loading='loading' color='primary' >
-            Status: <q-icon class='q-ml-sm' name='fas fa-check-circle'/>
-            <template v-slot:loading> <q-spinner class='on-left' /> Loading...  </template>
+          <q-btn color='primary' >
+            <div v-if='loading'>
+              <div> Loading...  <q-spinner class='on-right' /> </div>
+            </div>
+            <div v-else>
+              Status: <q-icon class='q-ml-sm' name='fas fa-check-circle'/>
+            </div>
           </q-btn>
-          <q-btn @click='onclick_screenshot' label='Screenshot' class='text-bold' color='blue-5' icon='fas fa-camera' >  </q-btn>
-          <q-btn @click='onclick_save_screenshot' label='Save Screenshot' class='text-bold' color='blue-5' icon='fas fa-download' >  </q-btn>
-          <q-btn @click='onclick_clear_canvas' label='Clear Canvas' class='text-bold' color='red-5' icon='fas fa-times' >  </q-btn>
+          <q-btn @click='onclick_screenshot' label='Screenshot' class='text-bold' color='blue-5' icon='fas fa-camera' dense />
+          <q-btn @click='onclick_save_screenshot' label='Save Screenshot' class='text-bold' color='blue-5' icon='fas fa-download' dense />
+          <q-btn @click='onclick_clear_canvas' label='Clear Canvas' class='text-bold' color='red-5' icon='fas fa-times' dense />
         </div>
       </div>
     </div>
@@ -35,11 +42,11 @@
 
     <div class='row q-col-gutter-sm q-pa-sm'>
       <div class='col-9'>
-        <q-chip class='q-ma-none q-my-sm' color='primary' text-color='white' icon='fas fa-cube' square> Canvas </q-chip>
-        <div ref='div_scene' style='width:100%;height:70vh'> </div>
+        <div ref='div_scene' style='width:100%;height:75vh'>
+          <q-chip class='absolute' label='3D World' color='primary' text-color='white' icon='fas fa-cube' square />
+        </div>
       </div>
       <div class='col-3'>
-        <q-chip class='q-ma-none q-my-sm' color='primary' text-color='white' icon='fas fa-bars' square> Menu </q-chip>
         <Menu ref='menu' class='full-width' :ctx='ctx' />
         <component :is='toolbox' v-bind='toolbox_props' class='full-width' :ctx='ctx'></component>
       </div>
